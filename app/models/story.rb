43c6a5cbffdb6c416ea54e112e26a649d97b7ff2
Kevin Bedell <kbedell@zipcar.com>
2015-06-06 17:01:27 -0400

    debug types

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,6 @@ class Story
   field :user, type: String
   field :story, type: String
   field :about_who, type: String
-  field :lat, type: Double
-  field :lon, type: Double
+  field :lat, type: Integer
+  field :lon, type: Integer
 end
