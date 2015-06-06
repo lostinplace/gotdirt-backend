b3df6a9116653f649b3f64c4f908e76bf25dd9c0
Kevin Bedell <kbedell@zipcar.com>
2015-06-06 17:06:13 -0400

    fix type

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post
   field :user, type: String
   field :story, type: String
   field :about_who, type: String
-  field :lat, type: Double
-  field :lon, type: Double
+  field :lat, type: Integer
+  field :lon, type: Integer
 end
