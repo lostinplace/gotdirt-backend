c7e9672288ce3ea3f0deb8f905ebd0763064c7b0
Vincent Toms <vtoms@vinnyt.org>
2015-06-06 18:55:16 -0400

    stories can respond with JSON?

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  respond_to :html, :json
 
   def index
     @stories = Story.all
