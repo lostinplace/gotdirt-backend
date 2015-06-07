8f73bff85f64817eccf54c8551af1bb43c364601
Kevin Bedell <kbedell@zipcar.com>
2015-06-07 11:19:34 -0400

    add tags

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,6 +3,7 @@ class Story
   field :user, type: String
   field :story, type: String
   field :about_who, type: String
+  field :tags, type: String
   field :lat, type: Float
   field :lon, type: Float
   field :date_time, type: DateTime
